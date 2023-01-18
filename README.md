79d9d7a6dead6ffc58038ff5f149c985b50b3c69
sam <samueltchilton@gmail.com>
2023-01-17 21:00:30 -0800

    aws lambda function basics

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # expressApp
 express app
+
+
+https://levelup.gitconnected.com/set-up-and-run-a-simple-node-server-project-38b403a3dc09
--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
-
-app.get('/', (req, res) => {
-    res.send('test')
-})
-
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+exports.handler = async (event, context) => {
+    var params = event["queryStringParameters"];
+    return `${params.temperature} , ${params.humidity}`;
+};
